17ddd66b31efe3c4428f5ad72a8a630c98b83851
Paola Garcia <paombe3@gmail.com>
2023-01-11 20:10:07 +0100

    9:50

---
+++ src/App.js
@@ -0,0 +1,28 @@
+//import "./App.css";
+
+const todos = [
+  { text: "Cortar cebolla", completed: false },
+  { text: "Tomar el curso de intro a react", completed: false },
+  { text: "Llorar con la llorona", completed: false },
+]
+
+function App() {
+  return (
+    <TodoCounter />
+      <h2>Has completado 2 de 3 TODOs</h2>
+
+    <TodoSearch />
+      <input placeholder="Cebolla" />
+
+    <TodoList>
+      {todos.map(todo => (
+        <TodoItem />
+      ))}
+    </TodoList>
+
+    <CreateTodoButton />
+        <button>+</button>
+  );
+}
+
+export default App;
