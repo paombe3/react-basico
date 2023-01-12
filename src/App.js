7233e6312f669542139af107f4e8600896292a8a
Paola Garcia <paombe3@gmail.com>
2023-01-12 23:24:56 +0100

    primera vista no funciona check verde no rojo hover

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { CreateTodoButton } from "./CreateTodoButton";
 //import "./App.css";
 
 const todos = [
-  { text: "Cortar cebolla", completed: false },
+  { text: "Cortar cebolla", completed: true },
   { text: "Tomar el curso de intro a react", completed: false },
   { text: "Llorar con la llorona", completed: false },
   { text: "LALALALALA", completed: false },
@@ -17,12 +17,15 @@ function App() {
   return (
     <React.Fragment>
       <TodoCounter />
-
       <TodoSearch />
 
       <TodoList>
         {todos.map((todo) => (
-          <TodoItem key={todo.text} text={todo.text} />
+          <TodoItem
+          key={todo.text} 
+          text={todo.text}
+          complete={todo.complete}
+          />
         ))}
       </TodoList>
 
