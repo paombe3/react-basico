822d4076cac383a2dd9778b5eb7718778ade78d8
Paola Garcia <paombe3@gmail.com>
2023-01-12 15:12:48 +0100

    creacion de archivos todos

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
 import React from "react";
+import { TodoCounter } from "./TodoCounter";
+import { TodoSearch } from "./TodoSearch";
+import { TodoList } from "./TodoList";
+import { TodoItem } from "./TodoItem";
+import { CreateTodoButton } from "./CreateTodoButton";
 //import "./App.css";
 
 const todos = [
   { text: "Cortar cebolla", completed: false },
   { text: "Tomar el curso de intro a react", completed: false },
   { text: "Llorar con la llorona", completed: false },
-]
+  { text: "LALALALALA", completed: false },
+];
 
 function App() {
   return (
     <React.Fragment>
-      {/*<TodoCounter />*/}
-        <h2>Has completado 2 de 3 TODOs</h2>
+      <TodoCounter />
 
-      {/*<TodoSearch />*/}
-        <input placeholder="Cebolla" />
+      <TodoSearch />
 
-      {/*<TodoList>
-        {todos.map(todo => (
-          <TodoItem />
+      <TodoList>
+        {todos.map((todo) => (
+          <TodoItem key={todo.text} text={todo.text} />
         ))}
-      </TodoList>*/}
+      </TodoList>
 
-      {/*<CreateTodoButton />*/}
-          <button>+</button>
+      <CreateTodoButton />
     </React.Fragment>
   );
 }
