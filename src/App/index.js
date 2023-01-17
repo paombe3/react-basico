ecb3113b4c5c5b9f225f1e9b534ff3120feb7a54
Paola Garcia <paombe3@gmail.com>
2023-01-17 21:28:28 +0100

    16 9

--- src/App/index.js
+++ src/App/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { AppUI } from "./AppUI";
-//import "./App.css";
 
 //const defaultTodos = [
 //  { text: "Cortar cebolla", completed: true },
@@ -9,86 +8,8 @@ import { AppUI } from "./AppUI";
 //  { text: "LALALALALA", completed: false },
 //];
 
-function useLocalStorage(itemName, initialValue) {
-  const [error, setError] = React.useState(false);
-  const [loading, setLoading] = React.useState(true);
-  const [item, setItem] = React.useState(initialValue);
-
-  React.useEffect(() => {
-    setTimeout(() => {
-      try {
-        const localStorageItem = localStorage.getItem(itemName);
-        let parsedItem;
-
-        if (!localStorageItem) {
-          localStorage.setItem(itemName, JSON.stringify(initialValue));
-          parsedItem = initialValue;
-        } else {
-          parsedItem = JSON.parse(localStorageItem);
-        }
-
-        setItem(parsedItem);
-        setLoading(false);
-      } catch (error) {
-        setError(error);
-      }
-    }, 1000);
-  });
-
-  const saveItem = (newItem) => {
-    try {
-      const stringifiedItem = JSON.stringify(newItem);
-      localStorage.setItem(itemName, stringifiedItem);
-      setItem(newItem);
-    } catch (error) {
-      setError(error);
-    }
-  };
-
-  return {
-    item,
-    saveItem,
-    loading,
-    error,
-  };
-}
-
 function App() {
-  const {
-    item: todos,
-    setItem: saveTodos,
-    loading,
-    error,
-  } = useLocalStorage("TODOS_V1", []);
-  const [searchValue, setSearchValue] = React.useState("");
-  const completedTodos = todos.filter((todo) => !!todo.completed).length;
-  const totalTodos = todos.length;
-
-  let searchedTodos = [];
-
-  if (!searchValue.length >= 1) {
-    searchedTodos = todos;
-  } else {
-    searchedTodos = todos.filter((todo) => {
-      const todoText = todo.text.toLowerCase();
-      const searchText = searchValue.toLowerCase();
-      return todoText.includes(searchText);
-    });
-  }
-
-  const completeTodo = (text) => {
-    const todoIndex = todos.findIndex((todo) => todo.text === text);
-    const newItem = [...todos];
-    newItem[todoIndex].completed = true;
-    saveTodos(newItem);
-  };
-
-  const deleteTodo = (text) => {
-    const todoIndex = todos.findIndex((todo) => todo.text === text);
-    const newItem = [...todos];
-    newItem.splice(todoIndex, 1);
-    saveTodos(newItem);
-  };
+  
 
   return [
     <AppUI
